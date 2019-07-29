df3ceaa5c1a2fce069f34fe21f08a2073a3eeb64
allynkalda <allyn.alda@gmail.com>
2019-07-29 12:16:04 +0200

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { CoursesComponent } from './courses/courses.component';
 import { CoursesService } from './courses.service';
 import { InputComponent } from './input/input.component';
 import { TitleCasePipe } from './title-case.pipe';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CoursesComponent, InputComponent, TitleCasePipe ],
+  declarations: [ AppComponent, HelloComponent, CoursesComponent, InputComponent, TitleCasePipe, FavoriteComponent ],
   bootstrap:    [ AppComponent ],
   providers: [CoursesService]
 })
